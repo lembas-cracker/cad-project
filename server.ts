572998884f098a8b24eff15430f423c90602631e
Ksenia Agalakova <agalaksu111@gmail.com>
2025-04-25 19:38:56 +0300

    Fixed path name in server.ts

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? ["https://https://bigcorp.netlify.app/"] //Netlify frontend URL
+        ? ["https://https://bigcorp.netlify.app"] //Netlify frontend URL
         : "*",
   })
 );
