7f3d4d67ebac4d4d981e3bc2af299121cdd233e7
Ksenia Agalakova <agalaksu111@gmail.com>
2025-04-25 12:55:38 +0300

    Implemented Express.js server

---
+++ src/server/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/contact", (req, res) => {
+  const { name, email, message } = req.body;
+
+  console.log("Name:", name);
+  console.log("Email:", email);
+  console.log("Message:", message);
+
+  setTimeout(() => {
+    res.json({
+      message: `Thank you for your interest, ${name}`,
+    });
+  }, 1000);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
