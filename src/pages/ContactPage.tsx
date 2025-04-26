498dab4d8554443ca53815c5f008369d1d898f05
Ksenia Agalakova <agalaksu111@gmail.com>
2025-04-26 15:58:29 +0300

    Fixed SEO meta tags

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,21 +1,11 @@
 import Form from "../components/Form.tsx";
 import Layout from "../components/Layout.tsx";
-import SEO from "../components/SEO.tsx";
 
 const ContactPage = () => {
   return (
-    <>
-      <SEO
-        title="Contact Us | Big Corp."
-        description="Fill out the form and learn more about us!"
-        name="Big Corp."
-        type="website"
-        image="https://i.imgur.com/aMLlgd7.png"
-      />
-      <Layout>
-        <Form />
-      </Layout>
-    </>
+    <Layout>
+      <Form />
+    </Layout>
   );
 };
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,24 +2,14 @@ import Features from "../components/Features.tsx";
 import Hero from "../components/Hero.tsx";
 import JoinUs from "../components/JoinUs.tsx";
 import Layout from "../components/Layout.tsx";
-import SEO from "../components/SEO.tsx";
 
 const HomePage = () => {
   return (
-    <>
-      <SEO
-        title="Welcome to Big Corp. | Big Corp."
-        description="Big Corp. solves all your problems!"
-        name="Big Corp."
-        type="website"
-        image="https://i.imgur.com/5h5hP9G.png"
-      />
-      <Layout home>
-        <Hero />
-        <Features />
-        <JoinUs />
-      </Layout>
-    </>
+    <Layout home>
+      <Hero />
+      <Features />
+      <JoinUs />
+    </Layout>
   );
 };
 
