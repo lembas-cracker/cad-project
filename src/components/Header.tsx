8cb995ef0445282f48ad75ef775a70c4f568bc04
Ksenia Agalakova <agalaksu111@gmail.com>
2025-04-27 16:24:23 +0300

    Added responsive styles for Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,12 +9,27 @@ const StyledAppBar = styled(AppBar)`
   top: 0;
   z-index: 1000;
   background-color: rgb(15, 70, 124);
+
+  @media (min-width: 1200px) {
+    padding-left: 128px;
+    padding-right: 128px;
+    transition: all 0.4s ease;
+
+    .MuiToolbar-root {
+      padding: 0;
+      transition: all 0.4s ease;
+    }
+    .MuiContainer-root {
+      padding: 0;
+      transition: all 0.4s ease;
+    }
+  }
 `;
 
 const Header = () => {
   return (
     <StyledAppBar>
-      <Container maxWidth="xl" sx={{ display: "flex" }}>
+      <Container maxWidth="xl" sx={{ display: "flex", mx: "auto" }}>
         <Toolbar sx={{ justifyContent: "space-between", width: "100%" }}>
           <Button sx={{ color: "white", fontSize: "1.5rem", fontWeight: "bold" }} component={Link} to="/">
             Big Corp.
